82c2bbc31c2501e5f64749f88dfafa12212aef8f
ecclesias <77536370+zeviel@users.noreply.github.com>
2022-11-19 18:23:10 +0800

    Update randstuff.py

--- src/randstuff.py
+++ src/randstuff.py
@@ -1,16 +1,16 @@
 import requests
 
 class RandStuff:
-	def __init__(self):
+	def __init__(self) -> None:
 		self.api = "https://randstuff.ru"
 		self.headers = {
 			"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.82 Safari/537.36",
 			"x-requested-with": "XMLHttpRequest"
 		}
 
-	def generate_random_joke(self):
+	def generate_random_joke(self) -> dict:
 		return requests.post(
-			f"{self.api}/joke/generate/",
+			f"{self.api}/joke/generate",
 			headers=self.headers).json()
 
 	def generate_random_number(
@@ -20,7 +20,7 @@ class RandStuff:
 			end: int = 100,
 			list: str = None,
 			unique: int = 0,
-			tz: int = -480):
+			tz: int = -480) -> dict:
 		data = {
 			"count": count,
 			"from": "range",
@@ -35,22 +35,22 @@ class RandStuff:
 			data=data,
 			headers=self.headers).json()
 
-	def generate_random_wisdom(self):
+	def generate_random_wisdom(self) -> dict:
 		return requests.post(
 			f"{self.api}/saying/generate/",
 			headers=self.headers).json()
 
-	def generate_random_fact(self):
+	def generate_random_fact(self) -> dict:
 		return requests.post(
 			f"{self.api}/fact/generate/",
 			headers=self.headers).json()
 
-	def generate_random_ticket(self):
+	def generate_random_ticket(self) -> dict:
 		return requests.post(
 			f"{self.api}/ticket/generate/",
 			headers=self.headers).json()
 
-	def get_random_ask(self, question: str):
+	def get_random_ask(self, question: str) -> dict:
 		data = {
 			"question": question
 		}
@@ -63,7 +63,7 @@ class RandStuff:
 			self,
 			length: int = 8,
 			numbers: int = 0,
-			symbols: int = 0):
+			symbols: int = 0) -> dict:
 		data = {
 			"length": length,
 			"numbers": numbers,
@@ -74,7 +74,7 @@ class RandStuff:
 			data=data,
 			headers=self.headers).json()
 
-	def generate_random_question(self):
+	def generate_random_question(self) -> dict:
 		return requests.post(
 			f"{self.api}/question/generate/",
 			headers=self.headers).json()
@@ -82,7 +82,7 @@ class RandStuff:
 	def answer_to_question(
 			self,
 			id: int,
-			number: int):
+			number: int) -> dict:
 		data = {
 			"id": id,
 			"number": number
@@ -92,7 +92,9 @@ class RandStuff:
 			data=data,
 			headers=self.headers).json()
 
-	def generate_random_nickname(self, numbers: int = 1):
+	def generate_random_nickname(
+			self,
+			numbers: int = 1) -> dict:
 		data = {
 			"numbers": numbers
 		}
@@ -101,7 +103,7 @@ class RandStuff:
 			data=data,
 			headers=self.headers).json()
 
-	def generate_random_city(self, country: str = "all"):
+	def generate_random_city(self, country: str = "all") -> dict:
 		data = {
 			"country": country
 		}
